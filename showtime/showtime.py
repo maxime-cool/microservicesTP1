d13741fe7cc7f51257e6081bc917e980a58adc27
Zuoyu Zhang <129968070+Zuoyu2524@users.noreply.github.com>
2023-09-20 16:27:43 +0200

    Update showtime.py

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -7,12 +7,31 @@ app = Flask(__name__)
 PORT = 3202
 HOST = '0.0.0.0'
 
-with open('{}/databases/times.json'.format("."), "r") as jsf:
+#showtime\databases\times.json
+
+with open('{}/showtime/databases/times.json'.format("."), "r") as jsf:
    schedule = json.load(jsf)["schedule"]
 
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+
+@app.route("/showtime", methods = ['GET'])
+def get_schedule():
+   res = make_response(jsonify(schedule),200)
+   return res
+
+@app.route("/showmovies/<date>", methods = ['GET'])
+def get_movies_bydate(date):
+   for elem in schedule:
+      if str(elem["date"]) == str(date):
+         res = make_response(jsonify(elem["movies"]),200)
+         return res
+   
+   res = make_response(jsonify({"error":"bad input parameter"}),400)
+   return res
+
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
