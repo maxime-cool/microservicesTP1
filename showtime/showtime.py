c306cc03ae105e774e5c72152bab30b63feabb2f
Zuoyu Zhang <129968070+Zuoyu2524@users.noreply.github.com>
2023-11-22 11:42:51 +0100

    Update showtime.py

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -5,7 +5,7 @@ from werkzeug.exceptions import NotFound
 app = Flask(__name__)
 
 PORT = 3202
-HOST = 'showtime'
+HOST = 'localhost'
 
 with open('{}/databases/times.json'.format("."), "r") as jsf:
    schedule = json.load(jsf)["schedule"]
